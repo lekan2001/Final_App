1ec51630810139cf03a7f540b9b7444ae9f27079
Olalekan  Adisa <adisaolalekan2016@gmail.com>
2020-07-14 00:44:36 -0500

    Parse Setup Completed

---
+++ Final_App/View Controller/LoginViewController.h
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.h
+//  Final_App
+//
+//  Created by Olalekan Abdurazaq Adisa on 7/14/20.
+//  Copyright © 2020 Facebook University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Final_App/View Controller/LoginViewController.m
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.m
+//  Final_App
+//
+//  Created by Olalekan Abdurazaq Adisa on 7/14/20.
+//  Copyright © 2020 Facebook University. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
