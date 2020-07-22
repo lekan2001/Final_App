11868ec4b2586b6282f2d89bc8e5b413c00058ba
Olalekan  Adisa <adisaolalekan2016@gmail.com>
2020-07-22 00:38:52 -0500

    Stock detail setup

---
+++ Final_App/View Controller/InvestDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  InvestDetailViewController.h
+//  Final_App
+//
+//  Created by Olalekan Abdurazaq Adisa on 7/21/20.
+//  Copyright © 2020 Facebook University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface InvestDetailViewController : UIViewController
+@property(nonatomic,strong) NSDictionary *result;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Final_App/View Controller/InvestDetailViewController.m
@@ -0,0 +1,59 @@
+//
+//  InvestDetailViewController.m
+//  Final_App
+//
+//  Created by Olalekan Abdurazaq Adisa on 7/21/20.
+//  Copyright © 2020 Facebook University. All rights reserved.
+//
+
+#import "InvestDetailViewController.h"
+
+@interface InvestDetailViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *stockFullName;
+
+@property (weak, nonatomic) IBOutlet UILabel *stockSymbol;
+
+@property (weak, nonatomic) IBOutlet UILabel *marketLabel;
+@property (weak, nonatomic) IBOutlet UILabel *marketPriceLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *marketChangeLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *marketChanePercentLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *PreviousMarketLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *ExchangeLabel;
+@property (nonatomic,strong) NSArray *results;
+
+@end
+
+@implementation InvestDetailViewController
+
+
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+       // [self fetchStock];
+        
+        
+    self.stockFullName.text = self.result[@"longName"];
+    NSLog(@"%@", self.result[@"longName"]);
+    
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
